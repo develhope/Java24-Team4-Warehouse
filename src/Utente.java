c0ba952f2f057a832aff1691646a793d7391dbe9
Simone Riso <ez.simone.riso@gmail.com>
2024-03-28 16:43:00 +0100

    Update Utente.java
    Bozza metodi di Utente

--- src/Utente.java
+++ src/Utente.java
@@ -1,12 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Utente {
     private int id;
     private String nome;
     private String cognome;
+    private Map<Integer, Boolean> carrello; // Mappa che tiene traccia degli elementi nel carrello
 
     public Utente(int id, String nome, String cognome) {
         this.id = id;
         this.nome = nome;
         this.cognome = cognome;
+        this.carrello = new HashMap<>();
     }
 
     public int getId() {
@@ -33,4 +38,21 @@ public abstract class Utente {
         this.cognome = cognome;
     }
 
+    // Metodo per aggiungere un elemento al carrello tramite IDProdotto
+    public void aggiungiAlCarrello(int id) {
+        carrello.put(id, true);
+    }
+
+    // Metodo per rimuovere un elemento dal carrello tramite IDProdotto
+    public void rimuoviDalCarrello(int id) {
+        carrello.remove(id);
+    }
+
+    // Metodo per finalizzare l'operazione di vendita
+    public void finalizzaVendita(Magazzino magazzino) {
+        for (int id : carrello.keySet()) {
+            magazzino.rimuoviProdotto(id); // Rimuove dal magazzino
+        }
+        carrello.clear(); // Svuota il carrello
+    }
 }
