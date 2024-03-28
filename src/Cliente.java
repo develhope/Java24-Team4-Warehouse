c2a36fbd61e580d7324ae72a838bbe14756d8f7a
Simone Riso <ez.simone.riso@gmail.com>
2024-03-28 16:42:35 +0100

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,5 @@
-public class Cliente extends Utente{
-    //TODO Carrello C
+public class Cliente extends Utente {
+    // TODO Carrello C
 
     public Cliente(int id, String nome, String cognome) {
         super(id, nome, cognome);
