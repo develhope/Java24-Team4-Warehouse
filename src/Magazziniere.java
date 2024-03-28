c579f04e2f35c56e20c6d1d2e7013b911d8ac69e
Simone Riso <ez.simone.riso@gmail.com>
2024-03-28 16:42:39 +0100

    Update Magazziniere.java

--- src/Magazziniere.java
+++ src/Magazziniere.java
@@ -1,5 +1,6 @@
 public class Magazziniere extends Utente {
     private String badge;
+
     public Magazziniere(int id, String nome, String cognome, String badge) {
         super(id, nome, cognome);
         this.badge = badge;
